1074b0c7eb1e65420cc5d8fd613ac83cef312207
HubiDev <lukas.huber7@gmail.com>
2023-09-14 18:50:42 +0200

    Added simple code to open window

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/go-gl/glfw/v3.3/glfw"
+	"runtime"
+)
+
+func init() {
+	runtime.LockOSThread()
+}
 
 func main() {
 	fmt.Println("Hello World")
+
+	err := glfw.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer glfw.Terminate()
+
+	window, err := glfw.CreateWindow(640, 480, "Testing", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	window.MakeContextCurrent()
+
+	for !window.ShouldClose() {
+		// Do OpenGL stuff.
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
 }
